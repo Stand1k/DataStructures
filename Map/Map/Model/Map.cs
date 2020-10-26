520d197890318cc5d067deeced4167c87a75efc8
Denis Yatsenko <yatsenko.denis46@gmail.com>
2020-10-26 22:38:45 +0200

    map

--- Map/Map/Model/Map.cs
+++ Map/Map/Model/Map.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Map.Model
 {
-    public class Map<TKey, TValue>
+    public class Map<TKey, TValue> : IEnumerable
     {
         private int size = 100;
         private Item<TKey, TValue>[] Items;
+        private List<TKey> Keys = new List<TKey>();
 
         public Map() 
         {
@@ -20,8 +22,14 @@ namespace Map.Model
         {
             var hash = GetHash(item.Key);
 
+            if(Keys.Contains(item.Key))
+            {
+                return;
+            }
+
             if(Items[hash] == null)
             {
+                Keys.Add(item.Key);
                 Items[hash] = item;
             }
             else
@@ -29,16 +37,17 @@ namespace Map.Model
                 var placed = false;
                 for(var i = hash; i < size; i++)
                 {
-                    if(Items[i].Key.Equals(item.Key))
+                    if (Items[i] == null)
                     {
-                        return; 
-                    }
-
-                    if(Items[i] == null)
-                    {
+                        Keys.Add(item.Key);
                         Items[i] = item;
                         placed = true;
                         break;
+                    }
+
+                    if (Items[i].Key.Equals(item.Key))
+                    {
+                        return;
                     }
                 }
 
@@ -46,24 +55,172 @@ namespace Map.Model
                 {
                     for (var i = 0; i < hash; i++)
                     {
+                        if (Items[i] == null)
+                        {
+                            Keys.Add(item.Key);
+                            Items[i] = item;
+                            placed = true;
+                            break;
+                        }
+
                         if (Items[i].Key.Equals(item.Key))
                         {
                             return;
                         }
+                    }
+                }
 
+                if(!placed)
+                {
+                    throw new Exception("Словарь заполнен");
+                }
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            var hash = GetHash(key);
+
+            if(!Keys.Contains(key))
+            {
+                return;
+            }
+
+            if (Items[hash] == null)
+            {
+                for (var i = 0; i < size; i++)
+                {
+                    if (Items[i] != null && Items[i].Key.Equals(key))
+                    {
+                        Items[i] = null;
+                        Keys.Remove(key);
+                        return;
+                    }
+                }
+
+                return;
+            }
+
+            if (Items[hash].Key.Equals(key))
+            {
+                Items[hash] = null;
+                Keys.Remove(key);
+            }
+            else
+            {
+                var placed = false;
+                for (var i = hash; i < size; i++)
+                {
+                    if (Items[i] == null)
+                    {
+                        return;
+                    }
+
+                    if (Items[i].Key.Equals(key))
+                    {
+                        Items[i] = null;
+                        Keys.Remove(key);
+                        return;
+                    }
+                }
+
+                if (!placed)
+                {
+                    for (var i = 0; i < hash; i++)
+                    {
                         if (Items[i] == null)
                         {
-                            Items[i] = item;
-                            break;
+                            return;
+                        }
+
+                        if (Items[i].Key.Equals(key))
+                        {
+                            Items[i] = null;
+                            Keys.Remove(key);
+                            return;
                         }
                     }
                 }
             }
         }
 
+        public TValue Search(TKey key)
+        {
+            var hash = GetHash(key);
+
+            if(!Keys.Contains(key))
+            {
+                return default(TValue);
+            }
+
+            if(Items[hash] == null)
+            {
+                foreach(var item in Items)
+                {
+                    if(item.Key.Equals(key))
+                    {
+                        return item.Value;
+                    }
+                }
+
+                return default(TValue);
+            }
+
+            if (Items[hash].Key.Equals(key))
+            {
+                return Items[hash].Value;
+            }
+            else
+            {
+                var placed = false;
+                for (var i = hash; i < size; i++)
+                {
+                    if (Items[i] == null)
+                    {
+                        return default(TValue);
+                    }
+
+                    if (Items[i].Key.Equals(key))
+                    {
+                        return Items[i].Value;
+                    }
+                }
+
+                if (!placed)
+                {
+                    for (var i = 0; i < hash; i++)
+                    {
+                        if (Items[i] == null)
+                        {
+                            return default(TValue);
+                        }
+
+                        if (Items[i].Key.Equals(key))
+                        {
+                            return Items[i].Value;
+                        }
+                    }
+                }
+            }
+
+            return default(TValue);
+        }
+
+
         private int GetHash(TKey key)
         {
             return key.GetHashCode() % size;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach(var item in Items)
+            {
+                if(item != null)
+                {
+                    yield return item;
+                }
+            }
         }
     }
 }
--- Map/Map/Program.cs
+++ Map/Map/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Map.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace Map
     {
         static void Main(string[] args)
         {
+            var map =  new Map<int, string>();
+            map.Add(new Item<int, string>(1,"One"));
+            map.Add(new Item<int, string>(1,"One"));
+            map.Add(new Item<int, string>(2,"Two"));
+            map.Add(new Item<int, string>(3,"Three"));
+            map.Add(new Item<int, string>(4,"Four"));
+            map.Add(new Item<int, string>(5,"Five"));
+            map.Add(new Item<int, string>(101,"Hundred and one"));
+
+            foreach(var item in map)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(map.Search(7) ?? "Not found");
+            Console.WriteLine(map.Search(3) ?? "Not found");
+            Console.WriteLine(map.Search(101) ?? "Not found");
+
+            map.Remove(7);
+            map.Remove(3);
+            map.Remove(1);
+            map.Remove(101);
+
+            foreach(var item in map)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
