e99a2c49952ea6fb8a3e2b0ad1634f470e4ee6ab
Denis Yatsenko <yatsenko.denis46@gmail.com>
2020-10-28 20:29:28 +0200

    BinaryTree

---
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node<T> : IComparable<T>
+        where T: IComparable
+    {
+        public T Data { get; private set; }
+        public Node<T> Left { get; private set; }
+        public Node<T> Right { get; private set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if(Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj is Node<T> item)
+            {
+                return Data.CompareTo(item );
+            }
+            else
+            {
+                throw new Exception("Type mismatch");
+            }
+        }
+
+        public int CompareTo(T other)
+        {
+            return Data.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
